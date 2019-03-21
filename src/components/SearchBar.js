abcff48a3293c1365dfb095639c65d40229c4aad
Haur Kang <haur.kang@gmail.com>
2019-03-21 16:37:30 +0000

    adds onInputChange function to searchbar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+  onInputChange(event) {
+      console.log(event.target.value);
+  }
+
   render() {
     return (
       <React.Fragment>
-        <form>
-            <input type="text"/>
-        </form>
+        <div className="ui segment">
+            <form className="ui form">
+                <div className="field">
+                    <label>Search</label>
+                    <input type="text" onChange={this.onInputChange}/>
+                </div>
+            </form>
+        </div>
       </React.Fragment>
     );
   }
