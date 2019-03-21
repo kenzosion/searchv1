2825e390c055e58c3a838d4bec366582acd62218
Haur Kang <haur.kang@gmail.com>
2019-03-21 14:38:22 +0000

    adds searchbar to app.js

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>Search bar</div>
+      </React.Fragment>
+    )
+  }
+}
