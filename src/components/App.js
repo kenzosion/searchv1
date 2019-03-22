7c114085b4f6e22808b9238a93518eecb1761bb9
Haur Kang <haur.kang@gmail.com>
2019-03-22 12:39:40 +0000

    adds authorization api key

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import axios from "axios";
+
 import SearchBar from './SearchBar';
+
 
 
 
  class App extends Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID bff7a6b08cbaafc84b4c5e73289b0b608c00d4655c6a6cd931b16322234bdf91"
+      }
+    });
   }
 
   render() {
